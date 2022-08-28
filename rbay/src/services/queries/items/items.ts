36dc95575828f797fe5666cad021f6f700d18e6e
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-08-28 12:20:50 -0700

    Section 16: More Practice with the E-Commerce App

--- rbay/src/services/queries/items/items.ts
+++ rbay/src/services/queries/items/items.ts
@@ -2,7 +2,7 @@ import type { CreateItemAttrs } from '$services/types';
 import { client } from '$services/redis';
 import { serialize } from './serialize';
 import { genId } from '$services/utils';
-import { itemsKey } from '$services/keys';
+import { itemsKey, itemsByViewsKey, itemsByEndingAtKey, itemsByPriceKey } from '$services/keys';
 import { deserialize } from './deserialize';
 
 export const getItem = async (id: string) => {
@@ -35,7 +35,21 @@ export const createItem = async (attrs: CreateItemAttrs, userId: string) => {
 
 	const serialized = serialize(attrs);
 
-	await client.hSet(itemsKey(id), serialized);
+	await Promise.all([
+		client.hSet(itemsKey(id), serialized),
+		client.zAdd(itemsByViewsKey(), {
+			value: id,
+			score: 0
+		}),
+		client.zAdd(itemsByEndingAtKey(), {
+			value: id,
+			score: attrs.endingAt.toMillis()
+		}),
+		client.zAdd(itemsByPriceKey(), {
+			value: id,
+			score: 0
+		})
+	]);
 
 	return id;
 };
